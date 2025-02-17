1a42911ef1c5aebce2a84d0e3f8fd7d1cc87f902
Hugo Mossberg <Hugo.mossberg1@live.com>
2025-02-17 20:04:55 +0100

    jkdjk

--- checkapi.py
+++
@@ -1,7 +0,0 @@
-import openai
-
-client = openai.OpenAI(api_key="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")
-
-models = client.models.list()
-for model in models:
-    print(model.id)
